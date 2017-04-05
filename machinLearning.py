d877e90eb22c279cc15de121aa4860c7b6214331
LumenH <aurelie.debrot@he-arc.ch>
2017-04-05 09:25:40 +0200

    first commit

---
+++ machinLearning.py
@@ -0,0 +1,10 @@
+import sklearn
+import sklearn.datasets
+
+#from shutil import copy2
+
+#Il faut prendre les textes du dossier tagged. C'estlà où ils sont taggés et sont sous formes infinitive.
+#1600 texts sont pour l'entraînement parmis 50% de négatifs, 50% de positif. Les 400 textes restant sont pour les tests. 50/50 de négatifs et positifs
+#Une fois que le tir est fait, on peut reprendre le truc de sklearn en adaptant les données
+
+#copy2(src, dst) #copie le fichier src dans le fichier dst ->path given as string, ça peut être un dossier
